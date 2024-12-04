1126da9bd88d7c1726afa3c2a5cde1f353079470
NONGSANG\nongsang <shdtkd37@gamil.com>
2024-12-04 20:20:41 +0900

    앱바 색깔 추가

--- static_screen/lib/main.dart
+++ static_screen/lib/main.dart
@@ -20,9 +20,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        // 앱바
         appBar: AppBar(
-          title: const Text('Stateless Test'),
+          title: const Center(
+            child: Text(
+              'Stateless Test',
+              style: TextStyle(color: Colors.white)
+            ),
+          ),
+          backgroundColor: Colors.blue,
         ),
+        
+        // 토글 버튼
         body: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -31,9 +40,11 @@ class MyApp extends StatelessWidget {
             children: [
               IconButton(onPressed: changeCheck,
                           color: Colors.red,
-                          icon: enabled? Icon(Icons.check_box, size: 20) : Icon(Icons.check_box_outline_blank, size: 20)),
+                          icon: enabled
+                              ? const Icon(Icons.check_box, size: 20) :
+                                const Icon(Icons.check_box_outline_blank, size: 20)),
 
-              Text('$strText', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+              Text(strText, style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
             ],
           )
         )
