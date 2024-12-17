a16fc3ed021bb410081ab111fae1dfd482facd7f
NONGSANG\nongsang <shdtkd37@gamil.com>
2024-12-17 23:53:09 +0900

    rootBundle, DefaultAssetBundle을 사용하여 에셋을 로드 및 화면에 출력

--- asset/lib/main.dart
+++ asset/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // rootBundle을 사용하기 위해 포함
 
 void main() {
   runApp(const MyApp());
@@ -6,6 +7,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // rootBundle을 사용한 텍스트 파일 읽어오기
+  // 비동기로 읽어온다.
+  Future<String> useRootBundle() async {
+    return await rootBundle.loadString('assets/text/my_text.txt');
+  }
+
+  // DefaultAssetBundle을 사용한 텍스트 파일 읽어오기
+  // 비동기로 읽어온다.
+  Future<String> useDefaultAssetBundle(BuildContext context) async {
+    return await DefaultAssetBundle.of(context).loadString('assets/text/my_text.txt');
+  }
 
   // This widget is the root of your application.
   @override
@@ -21,7 +34,26 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        body: Image.asset('images/icon/user.png'),  // images/icon/ 하위 폴더에 있는 user.png 에셋 불러오기
+        body: Column(
+          children: [
+            Image.asset('images/icon.jpg'),
+            Image.asset('images/icon/user.png'),
+            // 비동기로 로드한 에셋을 빌드
+            FutureBuilder(
+                future: useRootBundle(),  // rootBundle을 사용하여 에셋 로드
+                builder: (context, snapshot) {
+                  return Text('rootBundle: ${snapshot.data}'); // 에셋을 로드한 결과물을 snapshot에 전달하여 사용
+                }
+            ),
+            // 비동기로 로드한 에셋을 빌드
+            FutureBuilder(
+                future: useDefaultAssetBundle(context), // DefaultAssetBundle을 사용하여 에셋 로드
+                builder: (context, snapshot) {
+                  return Text('DefaultAssetBundle: ${snapshot.data}');  // 에셋을 로드한 결과물을 snapshot에 전달하여 사용
+                },
+            ),
+          ],
+        )
       ),
     );
   }
