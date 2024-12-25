025c48f5c5c6aa0e27f89e0246839dc28e94e386
NONGSANG\nongsang <shdtkd37@gamil.com>
2024-12-25 22:26:05 +0900

    mainAxisSize 기본 코드

---
+++ main_axis_size/android/app/src/main/kotlin/com/example/main_axis_size/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.main_axis_size
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ main_axis_size/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(
+            child: Text('Test', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+        body: Container(
+          color: Colors.yellow,
+          child: Row(
+            children: [
+              Container(
+                width: 50, height: 100, color: Colors.red,
+              ),
+              Container(
+                width: 50, height: 50, color: Colors.green,
+              ),
+              Container(
+                width: 50, height: 150, color: Colors.blue,
+              ),
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
