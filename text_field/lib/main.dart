01369fb30ffaec1abe016ad795c42c51b45fa8d0
NONGSANG\nongsang <shdtkd37@gamil.com>
2025-01-11 20:59:27 +0900

    사용자 입력이 발생할 때마다 호출할 함수 등록

--- text_field/lib/main.dart
+++ text_field/lib/main.dart
@@ -33,6 +33,27 @@ class TestScreen extends StatefulWidget {
 
 class TestState extends State<TestScreen> {
   final controller = TextEditingController(); // 텍스트 필드에 입력된 데이터를 저장할 수 있는 컨트롤러
+  int textCounter = 0;
+
+  // 입력이 발생할 때마다 호출할 함수
+  void _printValue() {
+    print('_printValue() : ${controller.text}');
+    setState(() {
+      textCounter = controller.text.length;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(_printValue); // 컨트롤러에 함수 등록
+  }
+
+  @override
+  void dispose() {
+    controller.dispose(); // 컨트롤러 삭제
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
