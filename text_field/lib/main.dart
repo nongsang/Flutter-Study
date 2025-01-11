20c0f86b8e5eeafffcb1667b7ddd96f4127afa16
NONGSANG\nongsang <shdtkd37@gamil.com>
2025-01-11 20:02:46 +0900

    Text Field로 유저 입력 기능

---
+++ text_field/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+
+        body: TextField(
+          style: TextStyle(fontSize: 15),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
