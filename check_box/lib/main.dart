979ff49c67a521005c602453a3e88ae75dfa0535
NONGSANG\nongsang <shdtkd37@gamil.com>
2025-01-13 17:17:31 +0900

    체크박스 예제 코드

---
+++ check_box/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: TestScreen(),
+      )
+    );
+  }
+}
+
+class TestScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TestState();
+  }
+}
+
+class TestState extends State<TestScreen> {
+  bool? isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          value : isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked = value;
+            });
+          },
+        ),
+        Text('checkbox value is $isChecked'),
+      ],
+    );
+  }
+}
