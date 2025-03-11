9083507dc1043b31c265067f06ca11df00edb25a
nongsang <shdtkd37@gmail.com>
2025-03-11 22:14:58 +0900

    isolate를 이용한 병렬처리 코드

--- isolate_basic/lib/main.dart
+++ isolate_basic/lib/main.dart
@@ -1,125 +1,87 @@
+import 'dart:async';
+import 'dart:isolate';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+myIsolate(SendPort mainPort) {
+  ReceivePort subPort = ReceivePort();  // 8. 메인 포트에 알려줄 서브 포트를 생성
+  mainPort.send({'port': subPort.sendPort});  // 9. 메인 포트로 서브 포트의 정보를 알려준다.
+  subPort.listen((message) {  // 10. 메인 아이솔레이트에서 전달하는 데이터를 처리하는 이벤트 등록
+    if (message['msg'] != 'bye') {  // 13. 메인 아이솔레이트에서 보내는 데이터가 bye가 아니라면
+      int count = message['msg'];
+      mainPort.send({'msg': count * count});  // 14. 메인 아이솔레이트로 계산 값을 전달한다.
+    } else {
+      subPort.close();  // 20. 메인 아이솔레이트에서 보내는 데이터가 bye라면 서브 포트를 닫는다.
+    }
+  });
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyAppState extends State<MyApp> {
+  String result = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void onPress() async {  // 2. 버튼이 눌리면 실행할 함수
+    ReceivePort mainPort = ReceivePort(); // 3. 메인 포트를 생성
+    await Isolate.spawn(myIsolate, mainPort.sendPort);  // 4. 서브 아이솔레이트를 생성하면서 메인 포트를 알려준다.
+
+    SendPort? subPort;  // 5. 서브 아이솔레이트에서 생성한 서브 포트에 대한 정보를 받을 포트 변수
+    mainPort.listen((message) { // 6. 서브 아이솔레이트에서, 메인 포트로 전달하는 데이터를 처리하는 함수 등록
+      if (message['port'] != null) {  // 10. 서브 아이솔레이트에서 생성한 포트에 대한 정보를 전달하면
+        subPort = message['port'];  // 11. 포트 연결
+      } else if (message['msg'] != null) {  // 15. 서브 아이솔레이트에서 메시지를 전달하면
+        setState(() {
+          result = 'msg : ${message['msg']}'; // 16. 메시지 저장 및 리빌드
+        });
+      }
+    });
+
+    int count = 0;
+    Timer.periodic(Duration(seconds: 1), (timer) {  // 7. 1초마다 실행할 이벤트 등록
+      if (++count < 6) {
+        subPort?.send({'msg': count}); // 12. 1초마다 {'msg' : count}의 map 형태 데이터로 서브 포트로 전달
+      } else {
+        subPort?.send({'msg': 'bye'});  // 18. 제한이 끝나면 {'msg' : 'bye'} 데이터를 보낸다.
+        mainPort.close(); // 19. 모든 작업이 끝났으므로 메인 포트를 닫는다.
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test',style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                result,
+                style: TextStyle(fontSize: 30),
+              ),
+              ElevatedButton(
+                child: Text('test1'),
+                onPressed: onPress, // 1. 버튼을 누른다.
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
